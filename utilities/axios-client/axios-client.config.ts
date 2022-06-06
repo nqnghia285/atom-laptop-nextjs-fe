3ad9f552cd95663e2e8b041c33033e4f32e6d494
Nghia Ngo Quang <nqnghia285@gmail.com>
2022-06-06 19:47:35 +0700

    new update

--- utilities/axios-client/axios-client.config.ts
+++ utilities/axios-client/axios-client.config.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+const baseURL = typeof window !== 'undefined' ? `${window.location.hostname}/api` : '/api'
+const headers = { 'Authorization': window?.localStorage.getItem('authorization') ?? '' }
+
 const axiosClient = axios.create({
-   baseURL: `/api`,
-   headers: { 'Authorization': window.localStorage.getItem('authorization') ?? '' },
+   baseURL,
+   headers,
    timeout: 1e4,
    withCredentials: true,
    signal: new AbortController().signal,
