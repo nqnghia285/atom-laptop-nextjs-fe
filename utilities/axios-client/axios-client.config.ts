42f1e454fb61a44cf3ab64244e84b2b0a7584a1b
Nghia Ngo Quang <nqnghia285@gmail.com>
2022-06-06 19:59:59 +0700

    new update

--- utilities/axios-client/axios-client.config.ts
+++ utilities/axios-client/axios-client.config.ts
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
-const baseURL = `https://atomlaptop.vercel.app/api`
-const headers = { 'Authorization': window?.localStorage.getItem('authorization') ?? '' }
-
 const axiosClient = axios.create({
-   baseURL,
-   headers,
+   baseURL: `https://atomlaptop.vercel.app/api`,
    timeout: 1e4,
    withCredentials: true,
    signal: new AbortController().signal,
